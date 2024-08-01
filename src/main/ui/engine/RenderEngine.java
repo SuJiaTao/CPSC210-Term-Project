98e903bc9ee065a421b64727161a898bb51d968a
JT <bailey.jt.brown@gmail.com>
2024-07-31 20:51:59 -0700

    working on/debugging - working on clipping - debugging because the UV interp for cutting these tris up is WRONG!!!

--- src/main/ui/engine/RenderEngine.java
+++ src/main/ui/engine/RenderEngine.java
@@ -5,6 +5,7 @@ import model.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.image.*;
+import java.util.ArrayList;
 import java.util.concurrent.locks.*;
 
 // Hosts the rendering logic code for ViewportPanel, functions similarly to ui.legacy's ViewportEngine class
@@ -93,11 +94,6 @@ public class RenderEngine implements Tickable {
     }
 
     private void drawPlanet(Planet planet) {
-        Vector3 planetWorldPos = Transform.multiply(viewTransform, planet.getPosition());
-        if (planetWorldPos.getZ() + planet.getRadius() + CLIPPING_TOLERANCE >= CLIPPING_PLANE_DEPTH) {
-            return;
-        }
-
         Vector3 planetScale = new Vector3(planet.getRadius(), planet.getRadius(), planet.getRadius());
         Transform planetTransform = Transform.transform(planet.getPosition(), new Vector3(), planetScale);
         Transform meshTransform = Transform.multiply(planetTransform, viewTransform);
@@ -154,14 +150,18 @@ public class RenderEngine implements Tickable {
     // EFFECTS: renders a given triangle
     private void shadeTriangle(AbstractShader shader, Triangle tri) {
         // NOTE:
-        // the standard procedure for rendering an arbitrary triangle is to split it in
-        // the middle, and render the flattop/flatbottom parts of it each
+        // the standard procedure for rendering an arbitrary triangle is to clip it,
+        // then split it in the middle, and render the flattop/flatbottom parts of it
+        // each
 
-        Triangle sortedTri = sortTriangleByHeight(tri);
-        Triangle[] cutTris = cutSortedTriangle(sortedTri);
+        Triangle[] clippedTris = clipTriangle(tri);
+        for (Triangle clippedTri : clippedTris) {
+            Triangle sortedTri = sortTriangleByHeight(clippedTri);
+            Triangle[] cutTris = cutSortedTriangle(sortedTri);
 
-        shadeTriangleFlatBottom(shader, cutTris[0], sortedTri);
-        shadeTriangleFlatTop(shader, cutTris[1], sortedTri);
+            shadeTriangleFlatBottom(shader, cutTris[0], sortedTri);
+            shadeTriangleFlatTop(shader, cutTris[1], sortedTri);
+        }
     }
 
     // MODIFIES: this
@@ -189,6 +189,10 @@ public class RenderEngine implements Tickable {
         float endY = flatBotTri.verts[0].getY();
         endY = Math.min(endY, (float) bufferSize);
 
+        if (!(Float.isFinite(startY) && Float.isFinite(endY))) {
+            return; // sanity check
+        }
+
         for (float drawY = startY; drawY <= endY; drawY += 1.0f) {
             float travelledY = drawY - flatBotTri.verts[2].getY();
 
@@ -197,6 +201,10 @@ public class RenderEngine implements Tickable {
 
             float endX = flatBotTri.verts[2].getX() + (travelledY * invSlopeRightToTop);
             endX = Math.min(endX, (float) bufferSize);
+
+            if (!(Float.isFinite(startX) && Float.isFinite(endX))) {
+                return; // sanity check
+            }
 
             for (float drawX = startX; drawX <= endX; drawX += 1.0f) {
                 prepareAndDrawFragment(shader, new Vector3(drawX, drawY, 0.0f), target);
@@ -372,6 +380,106 @@ public class RenderEngine implements Tickable {
         }
 
         return sorted;
+    }
+
+    // EFFECTS: cuts up a given triangle such that none of its verticies go past the
+    // clipping plane (this is highly non-trivial), and returns the generated
+    // triangles for when it has been clipped
+    private Triangle[] clipTriangle(Triangle tri) {
+        ArrayList<Integer> vertsBehind = new ArrayList<>();
+        ArrayList<Integer> vertsBefore = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (tri.verts[i].getZ() >= CLIPPING_PLANE_DEPTH) {
+                vertsBehind.add(i);
+            } else {
+                vertsBefore.add(i);
+            }
+        }
+        if (vertsBehind.size() == 3) {
+            return new Triangle[0]; // nothing to draw
+        }
+        if (vertsBehind.size() == 0) {
+            return new Triangle[] { tri }; // original triangle was fine
+        }
+        if (vertsBehind.size() == 1) {
+            return clipTriangleCase1(vertsBehind.get(0), vertsBefore.get(0), vertsBefore.get(1), tri);
+        }
+        if (vertsBehind.size() == 2) {
+            return clipTriangleCase2(vertsBefore.get(0), vertsBehind.get(0), vertsBehind.get(1), tri);
+        }
+
+        // should never reach here
+        throw new IllegalStateException();
+    }
+
+    // EFFECTS: clips a triangle in the case that only one of the inidices is behind
+    // the clipping plane
+    private Triangle[] clipTriangleCase1(int behind, int beforeL, int beforeR, Triangle original) {
+        // interpolate between beforeL to behind
+        float factorLToB = getClippingFactor(original.verts[beforeL], original.verts[behind]);
+        Vector3 vertexLToB = interpolateVector3(original.verts[beforeL], original.verts[behind], factorLToB);
+        Vector3 uvLToB = interpolateVector3(original.uvs[beforeL], original.uvs[behind], factorLToB);
+
+        // interpolate between beforeR to behind
+        float factorRToB = getClippingFactor(original.verts[beforeR], original.verts[behind]);
+        Vector3 vertexRToB = interpolateVector3(original.verts[beforeR], original.verts[behind], factorRToB);
+        Vector3 uvRToB = interpolateVector3(original.uvs[beforeR], original.uvs[behind], factorRToB);
+
+        // generate two new triangles, (L, LtoB, RtoB) and (L, R, RtoB)
+        Triangle triLeft = new Triangle();
+        triLeft.verts[0] = new Vector3(original.verts[beforeL]);
+        triLeft.uvs[0] = new Vector3(original.uvs[beforeL]);
+        triLeft.verts[1] = new Vector3(vertexLToB);
+        triLeft.uvs[1] = new Vector3(uvLToB);
+        triLeft.verts[2] = new Vector3(vertexRToB);
+        triLeft.uvs[2] = new Vector3(uvRToB);
+
+        Triangle triRight = new Triangle();
+        triRight.verts[0] = new Vector3(original.verts[beforeL]);
+        triRight.uvs[0] = new Vector3(original.uvs[beforeL]);
+        triRight.verts[1] = new Vector3(original.verts[beforeR]);
+        triRight.uvs[1] = new Vector3(original.uvs[beforeR]);
+        triRight.verts[2] = new Vector3(vertexRToB);
+        triRight.uvs[2] = new Vector3(uvRToB);
+
+        return new Triangle[] { triLeft, triRight };
+    }
+
+    // EFFECTS: clips a triangle in the case that two of the verticies are behind
+    // the clipping plane
+    private Triangle[] clipTriangleCase2(int before, int behindL, int behindR, Triangle original) {
+        float factorBToL = getClippingFactor(original.verts[before], original.verts[behindL]);
+        Vector3 vertexBToL = interpolateVector3(original.verts[before], original.verts[behindL], factorBToL);
+        Vector3 uvBToL = interpolateVector3(original.uvs[before], original.uvs[behindL], factorBToL);
+
+        float factorBToR = getClippingFactor(original.verts[before], original.verts[behindR]);
+        Vector3 vertexBToR = interpolateVector3(original.verts[before], original.verts[behindR], factorBToR);
+        Vector3 uvBToR = interpolateVector3(original.uvs[before], original.uvs[behindR], factorBToR);
+
+        // construct a triangle (B, BtoL, BtoR)
+        Triangle clipped = new Triangle();
+        clipped.verts[0] = new Vector3(original.verts[before]);
+        clipped.uvs[0] = new Vector3(original.uvs[before]);
+        clipped.verts[1] = new Vector3(vertexBToL);
+        clipped.uvs[1] = new Vector3(uvBToL);
+        clipped.verts[2] = new Vector3(vertexBToR);
+        clipped.uvs[2] = new Vector3(uvBToR);
+
+        return new Triangle[] { clipped };
+    }
+
+    // REQUIRES: behind to be behind the clipping plane and before to be before the
+    // clipping plane
+    // EFFECTS: gets the interpolation factor from before to behind that places a
+    // vertex right on the clipping plane
+    private float getClippingFactor(Vector3 before, Vector3 behind) {
+        return (CLIPPING_PLANE_DEPTH - before.getZ()) / (behind.getZ() - before.getZ());
+    }
+
+    // EFFECTS: interpolates a given vector3 from a to b based on an interpolation
+    // factor
+    private Vector3 interpolateVector3(Vector3 a, Vector3 b, float factor) {
+        return Vector3.add(Vector3.multiply(a, 1.0f - factor), Vector3.multiply(b, factor));
     }
 
     // EFFECTS: creates a new triangle which has been projected into screenspace
